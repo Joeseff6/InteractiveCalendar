b139805b14d67f7721d4bddb2ff6d00e37b6d87a
Joseph Soria <josephsoria6@gmail.com>
2020-12-24 12:36:22 -0600

    Edited text displaying date/time

--- Assets/script.js
+++ Assets/script.js
@@ -3,5 +3,5 @@ var date = dayjs().format('dddd D, YYYY');
 var currentTime = dayjs().format('hh:mmA');
 
 // Changing text content of the currentDay ID element to the current date and time
-$(`#currentDay`).text(`Today's date is ${date}, and the time at page refresh is ${currentTime}.`);
+$(`#currentDay`).text(`Today's date is ${date}, and the time at page refresh is ${currentTime}`);
 
