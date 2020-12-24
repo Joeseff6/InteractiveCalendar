9917fe3794026ebb4b6b3285cb27e614391372ed
Joseph Soria <josephsoria6@gmail.com>
2020-12-23 23:05:13 -0600

    Added comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,7 @@
-
 // Variables made using Day.js via CDN
 var date = dayjs().format('dddd D, YYYY');
 var currentTime = dayjs().format('hh:mmA');
 
-
+// Changing text content of the currentDay ID element to the current date and time
 $(`#currentDay`).text(`Today's date is ${date}, and the time at page refresh is ${currentTime}.`);
+
