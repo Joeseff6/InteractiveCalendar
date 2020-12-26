0bda560deeeb19511c49cfe2fa803991c7ef7ff6
Joseph Soria <josephsoria6@gmail.com>
2020-12-25 18:40:50 -0600

    Assigned buttons to a variable

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,9 @@
 // Variables made using Day.js via CDN
 var date = dayjs().format('dddd D, YYYY');
 var currentTime = dayjs().format('hh:mmA');
+var saveBtn = $(`[class='saveBtn']`);
+
+console.log(saveBtn);
 
 // Changing text content of the currentDay ID element to the current date and time
 $(`#currentDay`).text(`Today's date is ${date}, and the time at page refresh is ${currentTime}`);
