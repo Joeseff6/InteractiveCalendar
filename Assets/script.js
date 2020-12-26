e10283f3444a534b2e11a1a6520d86cb9d00f4c3
Joseph Soria <josephsoria6@gmail.com>
2020-12-26 14:01:24 -0600

    Refactored script

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,42 @@
 // Variables made using Day.js via CDN
 var date = dayjs().format('dddd D, YYYY');
 var currentTime = dayjs().format('hh:mmA');
-const timeForm = $(`[class='row']`);
-const saveBtn = $(`[class='saveBtn']`)
 var currentHour = parseInt(dayjs().format('H'));
 
-// Changing text content of the currentDay ID element to the current date and time
+// Other variables made for application using jQuery
+const timeForm = $(`[class='row']`);
+const saveBtn = $(`[class='saveBtn']`)
+var loggedEvents = [];
+
+// Create blank array for logged events
+for (let i = 0; i < 9; i++) {
+    loggedEvents.push("")
+}
+
+getStoredEvents()
+
+// Function to rendor local storage for data persistence
+function rendorStoredEvents() {
+    for (let i = 0; i < 9; i++) {
+        $(`[data-row=${i}]`).val(loggedEvents[i]);
+    }
+}
+
+// Function to retrieve stored events upon page open
+function getStoredEvents() {
+    var storedEvents = JSON.parse(localStorage.getItem(`Logged Events`))
+    if (storedEvents !== null) {
+        loggedEvents = storedEvents
+    }
+    rendorStoredEvents()
+}
+
+// Changing text content to display current day and time
 $(`#currentDay`).text(`Today's date is ${date}, and the time is ${currentTime}`);
 
 refreshTime()
 
+// Function to refresh time per second to update time live
 function refreshTime() {
     setInterval(function() {
         currentTime = dayjs().format('hh:mmA');
@@ -17,6 +44,7 @@ function refreshTime() {
     }, 1000)
 }
 
+// For loop to change row color based on the current hour
 let iterator = 0
 for (let i = 9; i < 18; i++) {
     if (i < currentHour) {
@@ -29,39 +57,28 @@ for (let i = 9; i < 18; i++) {
     iterator++
 }
 
-
-var loggedEvents = [];
-
-// Create blank array for logged events
-for (let i = 0; i < 9; i++) {
-    loggedEvents.push("")
-}
-
-// Retrieve stored events for data persistence on page refresh
-getStoredEvents()
-
-
-
+// Add functionality for when the save button is clicked
 saveBtn.click(function(event) {
-    // Grabs an index based on the save button's data-index number
     var buttonClicked = event.target
     var buttonIndex = buttonClicked.dataset.index
+    // Grabs specific button based on data-index attr of button clicked
     $(`[data-index=${buttonIndex}]`).text(`Event Saved!`)
     changeSaveText()
 
-
-    loggedEvents[buttonIndex] = $(`[data-row=${buttonIndex}]`).val();
-
+    // Submits user value to store into local storage
     timeForm.submit(function(event) {
+        // Assign user value the logged events array based on button clicked
+        loggedEvents[buttonIndex] = $(`[data-row=${buttonIndex}]`).val();
         event.preventDefault()
         storeEvent()
     })
 
-
+    // Function to store user values per submission
     function storeEvent() {
         localStorage.setItem(`Logged Events`, JSON.stringify(loggedEvents));
     }
 
+    // Function to change the save button text upon click
     function changeSaveText() {
         var textTimer = 2;
         var textInterval = setInterval(function() {
@@ -73,22 +90,3 @@ saveBtn.click(function(event) {
         }, 1000)
     }
 })
-
-function getStoredEvents() {
-    var storedEvents = JSON.parse(localStorage.getItem(`Logged Events`))
-
-    if (storedEvents !== null) {
-        loggedEvents = storedEvents
-    }
-
-    rendorStoredEvents()
-
-}
-
-function rendorStoredEvents() {
-    for (let i = 0; i < 9; i++) {
-        $(`[data-row=${i}]`).val(loggedEvents[i]);
-    }
-}
-
-
