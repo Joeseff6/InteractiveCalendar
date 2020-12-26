3979eee36b977733f6aab317abb4d277231f35a3
Joseph Soria <josephsoria6@gmail.com>
2020-12-25 23:38:42 -0600

    Completed function for data persistence

--- Assets/script.js
+++ Assets/script.js
@@ -3,12 +3,17 @@ const date = dayjs().format('dddd D, YYYY');
 const currentTime = dayjs().format('hh:mmA');
 const timeForm = $(`[class='row']`);
 const saveBtn = $(`[class='saveBtn']`)
-var loggedEvents = [];
+
+dayjs.extend(objectSupport)
+
 
 // Create blank array for logged events
 for (let i = 0; i < 9; i++) {
     loggedEvents.push("")
 }
+
+// Retrieve stored events for data persistence on page refresh
+getStoredEvents()
 
 // Changing text content of the currentDay ID element to the current date and time
 $(`#currentDay`).text(`Today's date is ${date}, and the time at page refresh is ${currentTime}`);
@@ -20,8 +25,6 @@ saveBtn.click(function(event) {
     var buttonIndex = buttonClicked.dataset.index
 
     loggedEvents[buttonIndex] = $(`[data-row=${buttonIndex}]`).val();
-    console.log(loggedEvents);
-
 
     timeForm.submit(function(event) {
         event.preventDefault()
@@ -34,5 +37,22 @@ saveBtn.click(function(event) {
     }
 })
 
+function getStoredEvents() {
+    var storedEvents = JSON.parse(localStorage.getItem(`Logged Events`))
+    console.log(storedEvents)
 
+    if (storedEvents !== null) {
+        loggedEvents = storedEvents
+    }
 
+    rendorStoredEvents()
+
+}
+
+function rendorStoredEvents() {
+    for (let i = 0; i < 9; i++) {
+        $(`[data-row=${i}]`).val(loggedEvents[i]);
+    }
+}
+
+dayjs.extend(objectSupport)
