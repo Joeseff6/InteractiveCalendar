e19552f45f618feb981d258a026d2dbf9a7d6594
Joseph Soria <josephsoria6@gmail.com>
2020-12-25 22:49:00 -0600

    Completed function when save button is clicked to store information per clicked row

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,38 @@
 // Variables made using Day.js via CDN
-var date = dayjs().format('dddd D, YYYY');
-var currentTime = dayjs().format('hh:mmA');
-var saveBtn = $(`[class='saveBtn']`);
+const date = dayjs().format('dddd D, YYYY');
+const currentTime = dayjs().format('hh:mmA');
+const timeForm = $(`[class='row']`);
+const saveBtn = $(`[class='saveBtn']`)
+var loggedEvents = [];
 
-console.log(saveBtn);
+// Create blank array for logged events
+for (let i = 0; i < 9; i++) {
+    loggedEvents.push("")
+}
 
 // Changing text content of the currentDay ID element to the current date and time
 $(`#currentDay`).text(`Today's date is ${date}, and the time at page refresh is ${currentTime}`);
+
+
+saveBtn.click(function(event) {
+    // Grabs an index based on the save button's data-index number
+    var buttonClicked = event.target
+    var buttonIndex = buttonClicked.dataset.index
+
+    loggedEvents[buttonIndex] = $(`[data-row=${buttonIndex}]`).val();
+    console.log(loggedEvents);
+
+
+    timeForm.submit(function(event) {
+        event.preventDefault()
+        storeEvent()
+    })
+
+
+    function storeEvent() {
+        localStorage.setItem(`Logged Events`, JSON.stringify(loggedEvents));
+    }
+})
+
+
 
