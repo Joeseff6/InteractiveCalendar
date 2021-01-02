51182e1de8239913aa2b4b197bd1aa56a3eb4dea
Joseph Soria <josephsoria6@gmail.com>
2021-01-02 14:34:58 -0600

    Refactored script

--- Assets/script.js
+++ Assets/script.js
@@ -5,15 +5,15 @@ var currentHour = parseInt(dayjs().format('H'));
 
 // Other variables made for application using jQuery
 const timeForm = $(`[class='row time-block']`);
-const saveBtn = $(`[class='col-2 saveBtn']`)
+const saveBtn = $(`[class='col-2 saveBtn']`);
 var loggedEvents = [];
 
 // Create blank array for logged events
 for (let i = 0; i < 9; i++) {
-    loggedEvents.push("")
+    loggedEvents.push("");
 }
 
-getStoredEvents()
+getStoredEvents();
 
 // Function to rendor local storage for data persistence
 function rendorStoredEvents() {
@@ -24,28 +24,28 @@ function rendorStoredEvents() {
 
 // Function to retrieve stored events upon page open
 function getStoredEvents() {
-    var storedEvents = JSON.parse(localStorage.getItem(`Logged Events`))
+    var storedEvents = JSON.parse(localStorage.getItem(`Logged Events`));
     if (storedEvents !== null) {
-        loggedEvents = storedEvents
+        loggedEvents = storedEvents;
     }
-    rendorStoredEvents()
+    rendorStoredEvents();
 }
 
 // Changing text content to display current day and time
 $(`#currentDay`).text(`Today's date is ${date}, and the time is ${currentTime}`);
 
-refreshTime()
+refreshTime();
 
 // Function to refresh time per second to update time live
 function refreshTime() {
     setInterval(function() {
         currentTime = dayjs().format('h:mmA');
-        $(`#currentDay`).text(`Today's date is ${date}, and the time is ${currentTime}`)
-    }, 1000)
+        $(`#currentDay`).text(`Today's date is ${date}, and the time is ${currentTime}`);
+    }, 1000);
 }
 
 // For loop to change row color based on the current hour
-let iterator = 0
+let iterator = 0;
 for (let i = 9; i < 18; i++) {
     if (i < currentHour) {
         $(`[data-row=${iterator}]`).addClass(`past`);
@@ -54,24 +54,24 @@ for (let i = 9; i < 18; i++) {
     } else {
         $(`[data-row=${iterator}]`).addClass(`future`);
     }
-    iterator++
+    iterator++;
 }
 
 // Add functionality for when the save button is clicked
 saveBtn.click(function(event) {
-    var buttonClicked = event.target
-    var buttonIndex = buttonClicked.dataset.index
+    var buttonClicked = event.target;
+    var buttonIndex = buttonClicked.dataset.index;
     // Grabs specific button based on data-index attr of button clicked
-    $(`[data-index=${buttonIndex}]`).text(`Event Saved!`)
-    changeSaveText()
+    $(`[data-index=${buttonIndex}]`).text(`Event Saved!`);
+    changeSaveText();
 
     // Submits user value to store into local storage
     timeForm.submit(function(event) {
-        event.preventDefault()
+        event.preventDefault();
         // Assign user value the logged events array based on button clicked
         loggedEvents[buttonIndex] = $(`[data-row=${buttonIndex}]`).val();
-        storeEvent()
-    })
+        storeEvent();
+    });
 
     // Function to store user values per submission
     function storeEvent() {
@@ -82,11 +82,11 @@ saveBtn.click(function(event) {
     function changeSaveText() {
         var textTimer = 2;
         var textInterval = setInterval(function() {
-            textTimer--
+            textTimer--;
             if (textTimer === 0) {
-                clearInterval(textInterval)
-                $(`[data-index=${buttonIndex}]`).text(`Save Event`)
+                clearInterval(textInterval);
+                $(`[data-index=${buttonIndex}]`).text(`Save Event`);
             }
-        }, 1000)
+        }, 1000);
     }
-})
+});
